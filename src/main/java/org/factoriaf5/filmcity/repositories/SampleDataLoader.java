43a6cb8723f500d2505eba2ad5d75cf1da0b2b6c
checkmyprojects <notnow.plz+github@gmail.com>
2022-04-13 13:24:31 +0200

    movies updated

--- src/main/java/org/factoriaf5/filmcity/repositories/SampleDataLoader.java
+++ src/main/java/org/factoriaf5/filmcity/repositories/SampleDataLoader.java
@@ -22,45 +22,75 @@ public class SampleDataLoader {
     public void addSampleMovies() {
         if (movieRepository.findAll().isEmpty()) {
             List<Movie> movies = List.of(
-                    new Movie("Jurassic Park",
-                            "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/oU7Oq2kFAAlGqbU4VoAE36g4hoI.jpg",
-                            "Steven Spielberg",
-                            1993,
-                            "A wealthy entrepreneur secretly creates a theme park featuring living dinosaurs drawn from prehistoric DNA.",null,false,0),
+                    new Movie("Sonic 2: La Película",
+                            "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/sJiHVM0A3OXDVxl4Z6a7ihMPHfm.jpg",
+                            "Jeff Fowler",
+                            2022,
+                            "Después de establecerse en Green Hills, Sonic se muere por demostrar que tiene madera de auténtico héroe, pero Robotnik regresa con nuevo compañero Knuckles, en busca de una esmeralda que tiene el poder de destruir civilizaciones, pero Sonic no esta solo lo ayudara Tails.",null,false,0),
                     new Movie("Ratatouille",
                             "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/npHNjldbeTHdKKw28bJKs7lzqzj.jpg",
                             "Brad Bird",
                             2007,
                             "Remy, a resident of Paris, appreciates good food and has quite a sophisticated palate. He would love to become a chef so he can create and enjoy culinary masterpieces to his heart's delight. The only problem is, Remy is a rat.",null,false,0),
-                    new Movie("Cruella",
-                            "https://pics.filmaffinity.com/Cruella-196211257-large.jpg",
-                            "Craig Gillespie",
+                    new Movie("Encanto",
+                            "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/d0ezQ1Jz0lpNsX1skEmIvqRL7mN.jpg",
+                            "Byron Howard",
                             2021,
-                            "Set in London during the punk rock movement of the 1970s, the film revolves around Estella Miller, an aspiring fashion designer, as she explores the path that will lead her to become a notorious up-and-coming fashion designer known as Cruella de Vil.",null,false,0
+                            "Los Madrigal son una familia extraordinaria que vive en una casa mágica de un pueblo vibrante en las montañas de Colombia, escondidas en un 'Encanto'. La magia ha bendecido a cada niño de la familia con un don único, desde la superfuerza hasta el poder de sanar.",null,false,0
                     ),
-                    new Movie("Mean Girls",
-                            "https://images-na.ssl-images-amazon.com/images/I/71eQtET-kmL._RI_.jpg",
-                            "Mark Waters",
-                            2004,
-                            "Lindsay Lohan stars as Cady Heron, a 16 year old homeschooled girl who not only makes the mistake of falling for Aaron Samuels (Jonathan Bennett), the ex-boyfriend of queenbee Regina George (Rachel McAdams), but also unintentionally joins The Plastics, led by Regina herself.",null,false,0
+                    new Movie("El bebé jefazo: Negocios de familia",
+                            "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/hvO2pc9X7kt7HZr5liSlk2MQA0Z.jpg",
+                            "Marla Frazee",
+                            2021,
+                            "Los hermanos Templeton se han convertido en adultos y se han alejado el uno del otro, pero un nuevo jefe bebé con un enfoque de vanguardia está a punto de unirlos nuevamente e inspirar un nuevo negocio familiar.",null,false,0
                     ),
-                    new Movie("Lady Bird",
-                            "https://pics.filmaffinity.com/Lady_Bird-546261513-large.jpg",
-                            "Greta Gerwig",
-                            2017,
-                            "Christine 'Lady Bird' McPherson (Saoirse Ronan) is a senior at a Catholic high school in Sacramento, California in 2002. She longs to attend a prestigious college in 'a city with culture'.",null,false,0
+                    new Movie("La patrulla canina: La película",
+                            "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/fOYt5TfJKHHz0hFY37kSXKvEdf7.jpg",
+                            "Bob Barlen",
+                            2021,
+                            "La patrulla canina está en racha. Cuando Humdinger, su mayor rival, se convierte en alcalde de la cercana Ciudad Aventura y empieza a causar estragos, Ryder y los heroicos cachorros se ponen en marcha para enfrentarse a este nuevo desafío.",null,false,0
                     ),
-                    new Movie("Suffragette",
-                            "https://musicart.xboxlive.com/7/b81f2600-0000-0000-0000-000000000002/504/image.jpg?w=1920&h=1080",
-                            "Sarah Gavron",
-                            2015,
-                            "Inspired by true events, Suffragette movingly explores the passion and heartbreak of those who risked all they had for women's right to vote – their jobs, their homes, their children, and even their lives.",null,false,0
-                    ),
-                    new Movie("On the basis of sex",
-                            "https://m.media-amazon.com/images/I/71TuUvNkS4L._SL1500_.jpg",
-                            "Mimi Leder",
+                    new Movie("Los Increíbles 2",
+                            "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/iNIKq8r155eRFlsoaxgfRYs71qY.jpg",
+                            "Brad Bird",
                             2018,
-                            "On the Basis of Sex is inspired by the true story of a young Ruth Bader Ginsburg – then a struggling attorney and new mother – who faces adversity and numerous obstacles in her fight for equal rights throughout her career.",null,false,0
+                            "Helen tiene que liderar una campaña para que los superhéroes regresen, mientras Bob vive su vida 'normal' con Violet, Dash y el bebé Jack-Jack —cuyos superpoderes descubriremos—. ",null,false,0
+                    ),
+                    new Movie("Shrek 2",
+                            "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/knRt4E8KyfwEv0SVu9LsLvD28IQ.jpg",
+                            "Kelly Asbury",
+                            2004,
+                            "Cuando Shrek y la princesa Fiona regresan de su luna de miel, los padres de ella los invitan a visitar el reino de Muy Muy Lejano para celebrar la boda. Para Shrek, al que nunca abandona su fiel amigo Asno, esto constituye un gran problema. Los padres de Fiona, por su parte, no esperaban que su yerno tuviera un aspecto semejante y, mucho menos, que su hija hubiera cambiado tanto.",null,false,0
+                    ),
+                    new Movie("Monstruos, S.A.",
+                            "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/jTE3BLVCpYaR71L2U8SHV22HvEr.jpg",
+                            "Pete Docter",
+                            2001,
+                            "Monsters Inc. es la mayor empresa de miedo del mundo, y James P. Sullivan es uno de sus mejores empleados. Asustar a los niños no es un trabajo fácil, ya que todos creen que los niños son tóxicos y no pueden tener contacto con ellos. Pero un día una niña se cuela sin querer en la empresa, provocando el caos.",null,false,0
+                    ),
+                    new Movie("El rey león",
+                            "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/b0MxU37dNmMwKtoPVYPKOZSIrIn.jpg",
+                            "Roger Allers",
+                            1994,
+                            "La sabana africana es el escenario en el que tienen lugar las aventuras de Simba, un pequeño león que es el heredero del trono. Sin embargo, se ve forzado a exiliarse al ser injustamente acusado de la muerte de su padre. Durante su destierro, hará buenas amistades y, finalmente, regresará para recuperar lo que legítimamente le corresponde.",null,false,0
+                    ),
+                    new Movie("Toy Story",
+                            "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/GdMDcacJra4ZmDaBIRASIpzjmU.jpg",
+                            "John Lasseter",
+                            1994,
+                            "Los juguetes de Andy, un niño de seis años, temen que haya llegado su hora y que un nuevo regalo de cumpleaños les sustituya en el corazón de su dueño. Woody, un vaquero que ha sido hasta ahora el juguete favorito de Andy, trata de tranquilizarlos hasta que aparece Buzz Lightyear, un héroe espacial dotado de todo tipo de avances tecnológicos.",null,false,0
+                    ),
+                    new Movie("Ice Age: La edad de hielo",
+                            "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/9rpdhn7p7mDZW2rUXsINf9c6sR5.jpg",
+                            "Chris Wedge",
+                            2002,
+                            "Hace muchos, muchos años, tantos como 20.000, una pequeña ardirata de nombre Scrat quería esconder una bellota en el hielo. Pero lo que hace es provocar una semicatástrofe que provoca que todos los animales migren hacia el sur, hacia tierras más cálidas. Bueno, lo que se dice todos, todos, no.",null,false,0
+                    ),
+                    new Movie("Buscando a Nemo",
+                            "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/jPhak722pNGxQIXSEfeWIUqBrO5.jpg",
+                            "Andrew Stanton",
+                            2003,
+                            "Nemo, un pececillo, hijo único muy querido y protegido por su padre, ha sido capturado en un arrecife australiano y ahora vive en una pecera en la oficina de un dentista de Sidney. El tímido padre de Nemo se embarcará en una peligrosa aventura para rescatar a su hijo. Pero Nemo y sus nuevos amigos tienen también un astuto plan para escapar de la pecera y volver al mar.",null,false,0
                     )
                     // Añadir aqui todas las pelis del catálogo!!
             );
